780a2e798152168423762131b7eddf7adb9a34f3
Kamron <khabibullayev5704@gmail.com>
2025-08-23 06:39:51 +0300

    bo'laqo'y

--- src/pages/OquvchilarXavfsizligi.jsx
+++ src/pages/OquvchilarXavfsizligi.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
